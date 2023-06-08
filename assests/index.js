49939fd4b2271bc009364e2ca94da1f6a0d37b7b
santhoshkumar <its.santhoshkumar@outlook.com>
2023-06-08 22:19:28 +0530

    converting into product

---
+++ assests/index.js
@@ -0,0 +1,44 @@
+function convertJpgToWebp() {
+    const input = document.getElementById('imageInput');
+    const file = input.files[0];
+
+    if (!file) {
+        showMessage('error', 'Please select an image file.');
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const img = new Image();
+
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            canvas.getContext('2d').drawImage(img, 0, 0);
+            const webpDataUrl = canvas.toDataURL('image/webp');
+
+            const link = document.createElement('a');
+            link.href = webpDataUrl;
+            link.download = 'converted.webp';
+            link.click();
+
+            URL.revokeObjectURL(webpDataUrl);
+            document.body.removeChild(link);
+
+            showMessage('success', 'Image converted successfully!');
+        };
+
+        img.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+}
+
+function showMessage(type, message) {
+    const resultMessage = document.getElementById('result-message');
+    resultMessage.textContent = message;
+    resultMessage.classList = type;
+}
